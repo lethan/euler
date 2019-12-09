8bdf6baf2765534f4e6872b0d05e9f3dc8a5b048
Kim Lethan <kim@skydning.dk>
2019-12-10 00:24:35 +0100

    Opgave 21

--- ruby/opgave021.rb
+++ ruby/opgave021.rb
@@ -4,3 +4,26 @@
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 # Evaluate the sum of all the amicable numbers under 10000.
+
+require 'prime'
+
+def proper_divisors(number)
+  return [] if number == 1
+
+  primes = number.prime_division.flat_map { |prime, freq| [prime] * freq }
+  (1...primes.size).each_with_object([1]) do |n, res|
+    primes.combination(n).map { |combi| res << combi.inject(:*) }
+  end.flatten.uniq
+end
+
+amicable_sum = 0
+
+divisor_sum = {}
+
+(1..10_000).each do |n|
+  sum = proper_divisors(n).sum
+  amicable_sum += n + sum if divisor_sum[sum] == n
+  divisor_sum[n] = sum
+end
+
+puts amicable_sum
